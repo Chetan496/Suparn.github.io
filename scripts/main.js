e8cc12742a73d086ed80890031f07e1d36afa880
Chetan Yewale <yewalechetan@gmail.com>
2016-05-23 23:57:15 +0530

    Updated the Objectives Section

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";smoothScroll.init(),$(document).ready(function(){$("ul.nav > li").click(function(t){t.preventDefault(),$("ul.nav > li").removeClass("active"),$(this).addClass("active")})});
+"use strict";smoothScroll.init(),$(document).ready(function(){$("ul.nav > li").click(function(e){e.preventDefault(),$("ul.nav > li").removeClass("active"),$(this).addClass("active")}),$("img.objectiveimg").click(function(e){e.preventDefault(),$("img.objectiveimg").removeClass("activeObjective"),$(this).addClass("activeObjective")})});
